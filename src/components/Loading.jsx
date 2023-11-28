705324ca64e3fd6e179c83ad982e3a00a44acccc
anthony <anthonycakins@gmail.com>
2023-11-28 11:38:12 +1000

    installed react icons & tailwindcss

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,11 +3,10 @@ import React from "react";
 export default function Loading() {
   return (
     <div
-      className="min-h-screen overflow-hidden w-full text-[#7342d6] flex justify-center items-center text-4xl"
+      className="overflow-hidden min-h-screen w-full text-[#7342d6] flex justify-center items-center text-4xl"
       style={{ backgroundColor: "rgba(115, 66, 214, 0.1)" }}
     >
-     d
+      <h1>Loading...</h1>
     </div>
   );
 }
-
